2b1687cefc56ca7bd06bc04642a168e6ffad5b41
Tom <heeley.thomas@gmail.com>
2019-05-08 14:49:09 +1200

    HealthBar Adddition
    Added a health bar for both players

---
+++ SlimeBrawl/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    bool isDead = false;
+    public Slider playerHealth;
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        takeDamage();
+        Dead();
+    }
+
+    void takeDamage()
+    {
+        if (Input.GetKeyDown("x"))
+        {
+            currentHealth -= 50;
+            playerHealth.value = currentHealth;
+        }
+
+    }
+
+    void Dead()
+    {
+        if (currentHealth <= 0 && isDead == false)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ SlimeBrawl/Assets/Scripts/player2Health.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player2Health : MonoBehaviour
+{
+    bool isDead = false;
+    public Slider playerHealth2;
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        takeDamage();
+        Dead();
+    }
+
+    void takeDamage()
+    {
+        if (Input.GetKeyDown("z"))
+        {
+            currentHealth -= 50;
+            playerHealth2.value = currentHealth;
+        }
+
+    }
+
+    void Dead()
+    {
+        if (currentHealth <= 0 && isDead == false)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
