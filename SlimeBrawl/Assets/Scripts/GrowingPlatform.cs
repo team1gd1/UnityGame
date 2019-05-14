10f4a335cdd4e263c162936828b97f78d29a6805
Sakyawira <sakyawira@gmail.com>
2019-05-14 12:38:35 +1200

    Start working on growing platforms and circle of death

---
+++ SlimeBrawl/Assets/Scripts/GrowingPlatform.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowingPlatform : MonoBehaviour
+{
+    public long MaxSize = 400000000000000;
+    private bool Growing = true; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Resize();
+    }
+
+    void Resize()
+    {
+        if (Growing == true)
+        {
+            transform.localScale += new Vector3(0.1f * Time.deltaTime, 0.1f * Time.deltaTime, 0);
+        }
+        else if (Growing == false)
+        {
+            transform.localScale -= new Vector3(0.1f * Time.deltaTime, 0.1f * Time.deltaTime, 0);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "PlatSpot")
+        {
+            if (Growing == true)
+            {
+                Growing = false;
+            }
+            else if (Growing == false)
+            {
+                Growing = true;
+            }
+        }
+    }
+}
