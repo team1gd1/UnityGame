aa58e9373f9d4aa2d922f46f505c3c3993613087
Jacobva <jacobva64@gmail.com>
2019-06-04 11:35:49 +1200

    More Grow code

--- SlimeBrawl/Assets/Scripts/PlayerGrow.cs
+++ SlimeBrawl/Assets/Scripts/PlayerGrow.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class PlayerGrow : MonoBehaviour
 {
 
     public Slider playerSize;
-    public int startingSize = 100;
-    public int currentSize;
+    public float startingSize = 100;
+    public float currentSize;
     public GameObject player;
+    public float pixelsPerUnit;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +23,12 @@ public class PlayerGrow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        grow();
+    }
+
+    void grow()
+    {
         currentSize = player.gameObject.GetComponent<PlayerHealth>().currentHealth;
-        
+        player.transform.localScale = new Vector3(currentSize, currentSize);
     }
 }
--- SlimeBrawl/Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ SlimeBrawl/Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -7,24 +7,13 @@ public class PlayerHealth : MonoBehaviour
 {
     bool isDead = false;
     public Slider playerHealth;
-<<<<<<< HEAD:SlimeBrawl/Assets/Scripts/PlayerScripts/PlayerHealth.cs
-<<<<<<< HEAD:SlimeBrawl/Assets/Scripts/PlayerHealth.cs
-    public int startingHealth = 100;
-    public int currentHealth;
+
+    public float startingHealth = 100;
+    public float currentHealth;
     public GameObject player;
-=======
-    public float startingHealth = 100;
-    public float currentHealth;
-=======
-    public float startingHealth = 100;
-    public float currentHealth;
-
-    public bool inCircle = false;
->>>>>>> 44e0fde3c2c18f5a18433a5dd2ccb6b0632ce5ad:SlimeBrawl/Assets/Scripts/PlayerScripts/PlayerHealth.cs
 
     public bool inCircle = false;
 
->>>>>>> 44e0fde3c2c18f5a18433a5dd2ccb6b0632ce5ad:SlimeBrawl/Assets/Scripts/PlayerScripts/PlayerHealth.cs
     // Start is called before the first frame update
     void Start()
     {
