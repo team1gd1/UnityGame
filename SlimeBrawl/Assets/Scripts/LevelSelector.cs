4ae9819897957326881c2ef080c90c9e6b8ea9ef
Sakyawira <sakyawira@gmail.com>
2019-06-04 12:00:20 +1200

    Created Main Menu and Main Menu UI

---
+++ SlimeBrawl/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public int SelectedLevel = 1;
+    public Text LevelName;
+    public GameObject LevelSelectorBG;
+
+    private int MaxLevel = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LevelName.text = "" + SelectedLevel;
+    }
+
+   public void AddLevel()
+    {
+        if (SelectedLevel < MaxLevel ) //&& SelectedLevel >= SaveSystem.LoadData().i_UnlockedLevels)
+        {
+            SelectedLevel = SelectedLevel + 1;
+        }
+    }
+
+  public  void SubtractLevel()
+    {
+        if (SelectedLevel > 1)
+        {
+            SelectedLevel = SelectedLevel - 1;
+        }
+    }
+
+    public void LoadLevel()
+    {
+       
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + SelectedLevel);
+    }
+
+    public void SwitchSelector()
+    {
+        if (!LevelSelectorBG.activeInHierarchy)
+        {
+            LevelSelectorBG.SetActive(true);
+        }
+        else
+        {
+            LevelSelectorBG.SetActive(false);
+        }
+    }
+
+}
---
+++ SlimeBrawl/Assets/Scripts/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Settings : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+        audioMixer.SetFloat("MasterVolume", volume);
+       // gameObject.
+            //GetComponent<AudioSource>().volume
+    }
+
+   
+}
